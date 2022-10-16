2265f38e671bbeb14a04b8fb76d5453422aab299
Ricardo Machado <ricardo_cabista@hotmail.com>
2022-10-15 23:16:44 -0300

    style: estilos adicionados a home, correçoes feitas

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express =  require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json())
 
 app.set('view engine','ejs')
 app.use(express.static('public'))
@@ -17,6 +20,15 @@ app.get('/Perguntar',(req,res)=>{
     res.render('Perguntar')
     
  })
+
+app.post('/ListaPerguntas',(req,res)=>{
+     const titulo = req.body.titulo;
+     const assunto = req.body.assunto;
+
+     res.send(`<div><h6>${titulo}</h6>
+                    <p>${assunto}</p>
+              </div>`)
+})
  
 
 app.listen(4000,(erro)=> console.log("app rodando"))
